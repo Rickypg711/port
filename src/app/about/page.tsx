46d24aaf3c8f193c17f9608e2f5f032d4691103b
Rickypg711 <77697094+Rickypg711@users.noreply.github.com>
2023-02-14 17:58:39 -0800

    progresss

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,25 +1,28 @@
 import Link from "next/link";
-
+import {
+  AiFillInstagram,
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+} from "react-icons/ai";
 export default function Home() {
   return (
     <div>
-
-        <div>
+      <div>
         <div className="flex mt-3 items-center">
-            <div className="ml-3 mr-7">
-                <Link href="/" className="text-teal-400 text-3xl">HOME</Link>
-                </div>
-                <div className="nav-line is-pink">
-                    </div>
-                    <div className=" flex-grow h-px bg-gray-900 mx-auto"></div>
-                    <div className="nav-left-item nav-copyright">
-            <div className=" ml-7 mr-3 text-teal-400 text-2xl">©/2022</div></div></div>
+          <div className="ml-3 mr-7">
+            <Link href="/" className="text-teal-400 text-3xl">
+              HOME
+            </Link>
+          </div>
+          <div className="nav-line is-pink"></div>
+          <div className=" flex-grow h-px bg-gray-900 mx-auto"></div>
+          <div className="nav-left-item nav-copyright">
+            <div className=" ml-7 mr-3 text-teal-400 text-2xl">©/2022</div>
+          </div>
         </div>
+      </div>
 
-
-       
-
-        {/*  */}
+      {/*  */}
       <section className="bg-white dark:bg-gray-900 py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
         <div className="max-w-3xl mx-auto">
           <h1 className="text-3xl font-bold font-serif text-gray-900 dark:text-white mb-5">
@@ -30,16 +33,16 @@ export default function Home() {
           </p>
           <div className="prose prose-gray dark:prose-white text-gray-800 dark:text-gray-400 mb-8">
             <p>
-              Im currently the <b>VP of Developer Experience at my own thing</b>,
-              where I lead our Developer Relations and Documentation teams. I
+              Im currently the <b>VP of Developer Experience at my own thing</b>
+              , where I lead our Developer Relations and Documentation teams. I
               focus on <b>educating and growing</b> communities.
             </p>
 
             <p>
-              Im passionate about many creative pursuits, including cooking music,
-              photography, and of course, coding. This combination
-              of interests is what ultimately led me to my current path in
-              building things as a developer in this communities.
+              Im passionate about many creative pursuits, including cooking
+              music, photography, and of course, coding. This combination of
+              interests is what ultimately led me to my current path in building
+              things as a developer in this communities.
             </p>
             <p>
               <b>I love</b> building for the web. From something as simple as a
@@ -49,28 +52,62 @@ export default function Home() {
               build.
             </p>
             <p>
-              Outside of Web development, I <b>angel invest</b> in developer tools
-              companies and <b>advise early-stage startups</b>. I also do
+              Outside of Web development, I <b>angel invest</b> in developer
+              tools companies and <b>advise early-stage startups</b>. I also do
               Developer Relations consulting work, helping companies take their
               DevRel function from 0 to 1, or provide guidance on growing
               communities, content creation, and developer marketing.
             </p>
           </div>
           <div className="flex flex-col md:flex-row md:items-center md:justify-between">
-            <a
+            {/* <Link
               href="https://twitter.com/leeerob"
               rel="noopener noreferrer"
               target="_blank"
               className="w-full md:w-auto mb-4 md:mb-0 md:mr-4 py-3 px-6 text-center text-white font-bold rounded-lg bg-blue-600 hover:bg-blue-700 transition-all"
             >
               Follow me on Twitter
-            </a>
-            <a
+            </Link> */}
+            <Link
               href="mailto:paredesricardog@gmail.com"
               className="w-full md:w-auto py-3 px-6 text-center text-gray-700 font-bold rounded-lg border border-gray-400 hover:border-blue-600 transition-all"
             >
               Get in touch
-            </a>
+            </Link>
+            {/*  */}
+
+            <div className="flex flex-row justify-center py-3 px-6 md:items-center md:justify-between">
+              <div className="mr-6">
+                <Link
+                  href="https://www.linkedin.com/in/ricardo-paredes-a2993a205/"
+                  className=" text-teal-400 hover:text-blue-200 text-3xl transition-all"
+                  target="_blank"
+                >
+                  <AiFillLinkedin />
+                </Link>
+              </div>
+
+              <div className="mr-6">
+                <Link
+                  href="https://twitter.com/Rickypg711"
+                  className="nav-left-link text-teal-400 text-3xl"
+                  target="_blank"
+                >
+                  <AiFillTwitterCircle />
+                </Link>
+              </div>
+              <div className="mr-6">
+                <Link
+                  href="https://www.instagram.com/rickyparedess/"
+                  className="nav-left-link text-teal-400 text-3xl"
+                  target="_blank"
+                >
+                  <AiFillInstagram />
+                </Link>
+              </div>
+            </div>
+
+            {/*  */}
           </div>
         </div>
       </section>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import Navbar from "./Components/Navbar";
+import Image from "next/image";
+import me from "../../public/IMG_0085.jpeg"
 
 export default function Home() {
   return (
@@ -7,28 +9,46 @@ export default function Home() {
         <div className="block pb-8 relative z-20">
       <Navbar />
       {/*  */}
-      <section className="topHero pt-12 leading-7 px-5 text-center">
-        <div className="content content-main">
+                                                                 {/* leading-7 */}
+
+<section className="topHero bg-white dark:bg-gray-900 py-8 md:py-16  px-5 text-center">
+        <div className="max-w-5xl mx-auto px-6 md:px-8 lg:px-10">
           <div className="header-mask">
-            <h1 className="header-my-name text-2xl pb-4 ">Ricardo Paredes</h1>
+            <h1 className="text-3xl md:text-4xl font-bold font-serif text-gray-800 dark:text-white mb-6" >
+                Ricardo Paredes
+                </h1>
           </div>
-          <div className="description-mask">
-            <p className="description transform ">
-              front end developer and aztec warrior
-              milo sucks
-              <br />
-            </p>
+                {/* <Image alt="Ricardo Paredes" src={me} width="150" height="150" className="rounded-full object-cover"/> */}
+
+          <div className="flex items-center space-x-4 md:space-x-8 mb-6 md:mb-8">
+                    <Image alt="Ricardo Paredes" src={me} width="150" height="150" className="hidden md:block rounded-full object-cover grayscale"/>
+
+          <p className="text-gray-800 dark:text-white text-lg font-medium max-w-lg">Hey, Im Ricardo. Im a <b>Web Developer</b> striving to build a faster, accessible and more beautiful web.</p>
+
           </div>
-          <div className="description-mask">
+          {/* <div className="description-mask">
             <p className="description transform">
               Currently the boss at <br /><Link href="https://www.lasvegas.com/" target="_blank" className="cursor-pointer text-teal-400">
                  Las vegas
               </Link>
               <br />
             </p>
-          </div>
+          </div> */}
         </div>
-      </section>
+    </section>
+{/*  */}
+
+      {/* <section className="bg-white dark:bg-gray-900 py-8 md:py-16">
+  <div className="max-w-5xl mx-auto px-6 md:px-8 lg:px-10">
+    <h1 className="text-3xl md:text-4xl font-bold font-serif text-gray-800 dark:text-white mb-6">
+        Lee Robinson</h1>
+    <div className="flex items-center space-x-4 md:space-x-8 mb-6 md:mb-8">
+      <Image alt="Ricardo Paredes" src={me} width="150" height="150" className="rounded-full object-cover"/>
+      <p className="text-gray-800 dark:text-white text-lg font-medium max-w-lg">Hey, Im Lee. Im the <b>VP of Developer Experience at Vercel</b> where my team helps developers build a faster web.</p>
+    </div>
+  </div>
+</section> */}
+
 
       {/*  */}
       <section className="bottom pt-11 pb-11 mb-2">
