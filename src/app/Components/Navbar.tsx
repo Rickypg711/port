7c68122b66365c5bdd49acf6f702dc1a3d71cac2
Rickypg711 <77697094+Rickypg711@users.noreply.github.com>
2023-02-14 18:45:01 -0800

    lil changes

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -9,7 +9,7 @@ import {
 export default function Navbar() {
   return (
     <div>
-      <div className="flex mt-3 items-center">
+      {/* <div className="flex mt-3 items-center">
         <div className="ml-2 mr-6">
           <Link
             href="https://www.linkedin.com/in/ricardo-paredes-a2993a205/"
@@ -18,9 +18,9 @@ export default function Navbar() {
           >
             <AiFillLinkedin />
           </Link>
-        </div>
+        </div> */}
         
-        <div className="mr-6">
+        {/* <div className="mr-6">
           <Link
             href="https://twitter.com/Rickypg711"
             className="nav-left-link text-teal-400 text-3xl"
@@ -42,7 +42,78 @@ export default function Navbar() {
         <div className=" text-red-300">
           <div className="  ml-16 mr-3 text-teal-400 text-2xl">©/2022</div>
         </div>
+      </div> */}
+
+     {/* ?] */}
+     <div>
+     <nav className="bg-white shadow-md">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+          <div className="flex-shrink-0 flex items-center">
+            <Link href="/" className="font-bold text-2xl">
+              Logo
+            </Link>
+          </div>
+          <div className="-mr-2 flex items-center md:hidden">
+            <button
+              type="button"
+              // onClick={toggleMenu}
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+            >
+              {/* {isOpen ? <FaTimes /> : <FaBars />} */}
+            </button>
+          </div>
+          <div className="hidden md:flex">
+            <div className="flex items-center">
+              <Link href="/" className="ml-10 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">
+                  Home
+                
+              </Link>
+              <Link href="/services" className="ml-10 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">
+                  Services
+                
+              </Link>
+              <Link href="/about" className="ml-10 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">
+                  About
+               
+              </Link>
+              <Link href="/contact" className="ml-10 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">
+                  Contact
+                
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
+      {/* {isOpen && (
+        <div className="md:hidden">
+          <div className="px-2 pt-2 pb-3">
+            <Link href="/">
+              <a className="block font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">
+                Home
+              </a>
+            </Link>
+            <Link href="/services">
+              <a className="block mt-1 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">
+                Services
+              </a>
+            </Link>
+            <Link href="/about">
+              <a className="block mt-1 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">
+                About
+              </a>
+            </Link>
+            <Link href="/contact">
+              <a className="block mt-1 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">
+                Contact
+              </a>
+            </Link>
+          </div>
+        </div>
+      )} */}
+    </nav>
+     </div>
+     {/*  */}
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,92 +1,72 @@
 import Link from "next/link";
 import Navbar from "./Components/Navbar";
 import Image from "next/image";
-import me from "../../public/IMG_0085.jpeg"
+import me from "../../public/IMG_0085.jpeg";
 
 export default function Home() {
   return (
-    <div >
-        <div className="block pb-8 relative z-20">
-      <Navbar />
-      {/*  */}
-                                                                 {/* leading-7 */}
+    <div>
+      <div className="block pb-8 relative z-20">
+        <Navbar />
+        {/*  */}
+        {/* leading-7 */}
 
-<section className="topHero bg-white dark:bg-gray-900 py-8 md:py-16  px-5 text-center">
-        <div className="max-w-5xl mx-auto px-6 md:px-8 lg:px-10">
-          <div className="header-mask">
-            <h1 className="text-3xl md:text-4xl font-bold font-serif text-gray-800 dark:text-white mb-6" >
+        <section className="topHero bg-white dark:bg-gray-900 py-8 md:py-16  px-5 text-center">
+          <div className="max-w-5xl mx-auto px-6 md:px-8 lg:px-10">
+            <div className="header-mask">
+              <h1 className="text-3xl md:text-4xl font-bold font-serif text-gray-800 dark:text-white mb-6">
                 Ricardo Paredes
-                </h1>
-          </div>
-                {/* <Image alt="Ricardo Paredes" src={me} width="150" height="150" className="rounded-full object-cover"/> */}
-
-          <div className="flex items-center space-x-4 md:space-x-8 mb-6 md:mb-8">
-                    <Image alt="Ricardo Paredes" src={me} width="150" height="150" className="hidden md:block rounded-full object-cover grayscale"/>
-
-          <p className="text-gray-800 dark:text-white text-lg font-medium max-w-lg">Hey, Im Ricardo. Im a <b>Web Developer</b> striving to build a faster, accessible and more beautiful web.</p>
-
-          </div>
-          {/* <div className="description-mask">
-            <p className="description transform">
-              Currently the boss at <br /><Link href="https://www.lasvegas.com/" target="_blank" className="cursor-pointer text-teal-400">
-                 Las vegas
-              </Link>
-              <br />
-            </p>
-          </div> */}
-        </div>
-    </section>
-{/*  */}
-
-      {/* <section className="bg-white dark:bg-gray-900 py-8 md:py-16">
-  <div className="max-w-5xl mx-auto px-6 md:px-8 lg:px-10">
-    <h1 className="text-3xl md:text-4xl font-bold font-serif text-gray-800 dark:text-white mb-6">
-        Lee Robinson</h1>
-    <div className="flex items-center space-x-4 md:space-x-8 mb-6 md:mb-8">
-      <Image alt="Ricardo Paredes" src={me} width="150" height="150" className="rounded-full object-cover"/>
-      <p className="text-gray-800 dark:text-white text-lg font-medium max-w-lg">Hey, Im Lee. Im the <b>VP of Developer Experience at Vercel</b> where my team helps developers build a faster web.</p>
-    </div>
-  </div>
-</section> */}
-
-
-      {/*  */}
-      <section className="bottom pt-11 pb-11 mb-2">
-        <div className="section-right text-center">
-          <div className="nav">
-            
-
-            {/* aboiut */}
-            <div className="nav-item">
-              <Link 
-              href="/about" 
-              className="nav-link text-7xl">
-                About
-              </Link>
+              </h1>
             </div>
 
-            {/* work */}
-            <div className="nav-item">
-              <Link 
-              href="/work" 
-              className="nav-link text-7xl text-green-300">
-                Work
-              </Link>
+            <div className="flex items-center space-x-4 md:space-x-8 mb-6 md:mb-8">
+              <Image
+                alt="Ricardo Paredes"
+                src={me}
+                width="150"
+                height="150"
+                className="hidden md:block rounded-full object-cover grayscale"
+              />
+
+              <p className="text-gray-800 dark:text-white text-lg font-medium max-w-lg">
+                Hey, Im Ricardo. Im a <b>Web Developer</b> striving to build a
+                faster, accessible and more beautiful web.
+              </p>
             </div>
-
-            {/* contact */}
-            <div className="nav-item">
-              <Link 
-              href="/contact" 
-              className="nav-link text-7xl  text-red-300 ">
-                Contact
-              </Link>
-            </div>
-
-
           </div>
-        </div>
-      </section>
+        </section>
+        {/*  */}
+
+        {/*  */}
+        <section className="bottom pt-11 pb-11 mb-2">
+          <div className="section-right text-center">
+            <div className="nav">
+              {/* aboiut */}
+              <div className="nav-item">
+                <Link href="/about" className="nav-link text-7xl">
+                  About
+                </Link>
+              </div>
+
+              {/* work */}
+              <div className="nav-item">
+                <Link href="/work" className="nav-link text-7xl text-green-300">
+                  Work
+                </Link>
+              </div>
+
+              {/* contact */}
+              <div className="nav-item">
+                <Link
+                  href="/contact"
+                  className="nav-link text-7xl  text-red-300 "
+                >
+                  Contact
+                </Link>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
